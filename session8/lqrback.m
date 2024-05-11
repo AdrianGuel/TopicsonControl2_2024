c64521863ac70251b3fca09547895a7df187d65c
A.-J. Guel-Cortez <52336176+AdrianGuel@users.noreply.github.com>
2024-05-11 20:08:01 +0100

    Update lqrback.m

--- session8/lqrback.m
+++ session8/lqrback.m
@@ -1,22 +1,36 @@
 close all;
 clearvars;
 
-A=[[2,1];[-1,1]]; B=[0;1]; 
-x0=[2;3];
-P=zeros(2,2,10);
-K=zeros(1,2,9);
-x=zeros(2,1,11); x(:,:,1)=x0;
+A=[2 1;-1 1];B=[0;1];x0=[2;-3];P=zeros(2,2,10);K=zeros(1,2,9);
+x=zeros(2,1,11);x(:,:,1)=x0;
+P(:,:,10)=[5 0;0 5];R=2;Q=[2 0;0 0.1];
 t=0:1:10;
-%% initial conditions
-P(:,:,10)=[[5,0];[0,5]]; R=2;
-Q=[[2,0];[0,0.1]];
 
 for i=9:-1:1
-    K(:,:,i)=inv(R+B'*P(:,:,i+1)*B)*B'*P(:,:,i+1)*A;
+    K(:,:,i)=(R+B'*P(:,:,i+1)*B)\B'*P(:,:,i+1)*A;
     P(:,:,i)=(A-B*K(:,:,i))'*P(:,:,i+1)*(A-B*K(:,:,i))+...
         Q+K(:,:,i)'*R*K(:,:,i);
 end
+for i=1:9
+    x(:,:,i+1)=A*x(:,:,i)-B*K(:,:,i)*x(:,:,i);
+end
 
+figure
+plot(t,squeeze(x(2,1,:)))
+hold on
+plot(t,squeeze(x(1,1,:)))
+
+figure
+plot(t(1:end-2),squeeze(K(1,1,:)))
+hold on
+plot(t(1:end-2),squeeze(K(1,2,:)))
+
+figure
+plot(t(1:end-1),squeeze(P(1,1,:)))
+hold on
+plot(t(1:end-1),squeeze(P(2,2,:)))
+hold on
+plot(t(1:end-1),squeeze(P(1,2,:)))
 for i=1:9
     x(:,:,i+1)=A*x(:,:,i)-B*K(:,:,i)*x(:,:,i+1);
 end
