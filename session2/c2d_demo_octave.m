b2b0aae3cf04214b4772a30b1f31ffa41026f78a
A.-J. Guel-Cortez <52336176+AdrianGuel@users.noreply.github.com>
2025-03-29 11:55:49 +0000

    Create c2d_demo_octave.m

---
+++ session2/c2d_demo_octave.m
@@ -0,0 +1,39 @@
+pkg load control  % Asegúrate de tener este paquete instalado
+
+% Define a transfer function: G(s) = 1 / (s^2 + 3s + 2)
+num = [1];
+den = [1 3 2];
+
+% Convert to state-space (tf2ss)
+[A, B, C, D] = tf2ss(num, den);
+
+disp('A ='), disp(A)
+disp('B ='), disp(B)
+disp('C ='), disp(C)
+disp('D ='), disp(D)
+
+% Sampling time
+T = 0.1;
+
+% Compute matrix exponential expm(A*T)
+Ad = expm(A*T);
+disp('Ad = expm(A*T) ='), disp(Ad)
+
+% Discretize the system (c2d with ZOH)
+sys_c = ss(A, B, C, D);
+sys_d = c2d(sys_c, T, 'zoh');
+
+% Display discrete system
+disp('Discrete A ='), disp(sys_d.A)
+disp('Discrete B ='), disp(sys_d.B)
+disp('Discrete C ='), disp(sys_d.C)
+disp('Discrete D ='), disp(sys_d.D)
+
+% Optional: plot step responses
+figure;
+step(sys_c);
+title('Continuous-time Step Response');
+
+figure;
+step(sys_d);
+title('Discrete-time Step Response');
